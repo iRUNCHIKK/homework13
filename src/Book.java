9156f3fa8820e18c4afa475ce4d2bef8237127dc
iRUNCHIKK <btkv_irina@icloud.com>
2024-05-16 00:16:51 +0500

    Initial commit

--- src/Book.java
+++ src/Book.java
@@ -28,15 +28,4 @@ public class Book {
     public String toString (){
         return "Название книги: " + name + ", год публикации: " + publicationYear + ", автор: " + author;
     }
-
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Book book = (Book) object;
-        return publicationYear == book.publicationYear && java.util.Objects.equals(name, book.name) && java.util.Objects.equals(author, book.author);
-    }
-
-    public int hashCode() {
-        return java.util.Objects.hash(name, author, publicationYear);
-    }
 }
